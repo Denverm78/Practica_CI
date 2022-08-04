b6d01a30640493ea65daf270205c0fac6c790ca1
Denis <denverm@yandex.ru>
2022-08-04 21:26:19 +0300

    Add if else

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") //переводит все буквы в строчные, чтобы можно было набирать имя Маша любыми буквами
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
