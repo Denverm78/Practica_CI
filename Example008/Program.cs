39cf5786d4c62f5fabdc76c1072efbdd81853918
Denis <denverm@yandex.ru>
2022-08-08 20:03:39 +0300

    add Example008

---
+++ Example008/Program.cs
@@ -0,0 +1,20 @@
+﻿int a1 = 13;
+int b1 = 15;
+int c1 = 34;
+int a2 = 3;
+int b2 = 76;
+int c2 = 18;
+int a3 = 34;
+int b3 = 55;
+int c3 = 21;
+
+int max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+Console.WriteLine(max);
